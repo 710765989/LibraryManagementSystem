73555f65007fa3e9ad9cc49dcb17586ea6a3b451
710765989 <710765989@qq.com>
2022-10-12 01:01:18 +0800

    创建实体类

---
+++ src/main/java/generator/domain/utils/Constant.java
@@ -0,0 +1,26 @@
+package generator.domain.utils;
+
+/**
+ * 常量类
+ */
+public class Constant {
+    /**
+     * 借阅状态 0:未借出 1:已借出
+     * book.status
+     */
+    public static final String BOOK_STATUS_0 = "0";
+    public static final String BOOK_STATUS_1 = "1";
+
+    /**
+     * 用户类型 0:管理员 1:读者
+     * user.type
+     */
+    public static final String USER_TYPE_MANAGER = "0";
+    public static final String USER_TYPE_READER = "1";
+
+    /**
+     * 归还状态 0:已归还 1:未归还
+     */
+    public static final String BORROW_STATUS_0 = "0";
+    public static final String BORROW_STATUS_1 = "1";
+}
