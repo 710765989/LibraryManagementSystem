54fb6f6f2dc7a2ad58eca7b58e00c71027153fa2
710765989 <710765989@qq.com>
2022-11-26 01:14:18 +0800

    图书馆管理系统后端

--- src/main/java/com/library/librarymanagementsystem/controller/LoginController.java
+++ src/main/java/com/library/librarymanagementsystem/controller/LoginController.java
@@ -1,6 +1,5 @@
 package com.library.librarymanagementsystem.controller;
 
-import com.library.librarymanagementsystem.base.service.impl.UserServiceImpl;
 import com.library.librarymanagementsystem.domian.entity.User;
 import com.library.librarymanagementsystem.service.impl.LoginServiceImpl;
 import com.library.librarymanagementsystem.utils.R;
